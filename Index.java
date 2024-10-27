8f088da322ad6b7e9701373032b82fc826e30e8d
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-10-27 12:55:08 +0100

    Update Index.java

--- Index.java
+++ Index.java
@@ -17,27 +17,46 @@ public class Index extends Application {
 	public void start(Stage primaryStage) {
 		// Créer un conteneur VBox
 		VBox root1 = new VBox(5);
-		// Créer un conteneur HBox
+		root1.getStyleClass().add("background");
+		// Créer des conteneurs HBox
 		HBox root2 = new HBox(5);
+		HBox barNavigation = new HBox(150);
+		barNavigation.getStyleClass().add("backgroundbarre");
 		// Créer un label
 		Label label = new Label("CraigsList");
 		// Créer un bouton poster une annonce
 		Button add = new Button("Déposez votre annonce en un clic !");
+		add.getStyleClass().add("addbutton");
 		// Créer une image 
-		Image im1 = new Image(getClass().getResourceAsStream("/Images/logo.png"));
+		Image im1 = new Image(getClass().getResourceAsStream("/Images/logo_trans.png"));
 		ImageView imView = new ImageView(im1);
 		// Propriétés de l'image
-		imView.setFitWidth(30);
-        	imView.setFitHeight(30);
+		imView.setFitWidth(50);
+        	imView.setFitHeight(50);
 		// Propriétés du label
-		label.getStyleClass().add("label");
+		label.getStyleClass().add("labeltitre");
 		label.setGraphic(imView);
-		label.setGraphicTextGap(50);
+		label.setGraphicTextGap(5);
 		label.setContentDisplay(ContentDisplay.LEFT);
 		// Ajouter les éléments au conteneur HBox
 		root2.getChildren().addAll(imView, label);
+		// Créer des labels pour la barre de navigation 
+		Label service = new Label("Services");
+		Label emploi = new Label("Emplois");
+		Label forum = new Label("Forums");
+		Label vendre = new Label("A vendre");
+		Label immo = new Label("Immobilier");
+		Label cv = new Label("CV");
+		service.getStyleClass().add("labelbarre");
+		emploi.getStyleClass().add("labelbarre");
+		forum.getStyleClass().add("labelbarre");
+		vendre.getStyleClass().add("labelbarre");
+		immo.getStyleClass().add("labelbarre");
+		cv.getStyleClass().add("labelbarre");
+		// Ajouter les labels à la barre de navigation 
+		barNavigation.getChildren().addAll(service, emploi, forum, vendre, immo, cv);
 		// Ajouter les éléments au conteneur VBox
-		root1.getChildren().addAll(root2, add);
+		root1.getChildren().addAll(root2, barNavigation, add);
 		// Créer la scène
 		Scene scene = new Scene(root1, 1500, 700);
 		// Configurer la scène principale
