c7ad9d746b964b963799c4e5a4cad665e4a928ec
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-10-30 18:30:47 +0100

    Update Index.java

--- Index.java
+++ Index.java
@@ -19,15 +19,32 @@ public class Index extends Application {
 	@Override
 	
 	public void start(Stage primaryStage) {
-		// Créer un conteneur VBox
+		// Créer le conteneur VBox principal
 		VBox root1 = new VBox(5);
-		root1.setPadding(new Insets(15, 10, 0, 10));
 		root1.getStyleClass().add("background");
+
+		// Créer le conteneur VBox de droite pour le HBox central 
+		VBox root3 = new VBox(30);
 		
-		// Créer des conteneurs HBox
-		HBox root2 = new HBox(5);
-		HBox barNavigation = new HBox(150);
+		// Créer le conteneur VBox de gauche pour le HBox central 
+		VBox root4 = new VBox();
+		
+		// Créer le conteneur HBox du haut 
+		HBox root2 = new HBox();
+		root2.setPadding(new Insets(15, 10, 0, 10));
+
+		// Créer le contenur HBox barre de navigation 
+		HBox barNavigation = new HBox(120);
 		barNavigation.getStyleClass().add("backgroundbarre");
+
+		// Créer le conteneur HBox central
+		HBox centre = new HBox(100);
+		centre.setPadding(new Insets(30, 30, 30, 30));
+		
+		// Créer le conteneur HBox footer 
+		HBox footer = new HBox(230);
+		footer.setPadding(new Insets(15, 0, 15, 0));
+		footer.getStyleClass().add("backgroundfooter");
 		
 		// Créer un label
 		Label label = new Label("CraigsList");
@@ -86,10 +103,54 @@ public class Index extends Application {
 		Region rightSpacer2 = new Region();
 		
 		// Ajouter les labels à la barre de navigation 
+		HBox.setHgrow(leftSpacer2, Priority.ALWAYS);
+		HBox.setHgrow(rightSpacer2, Priority.ALWAYS);
 		barNavigation.getChildren().addAll(leftSpacer2, service, emploi, forum, vendre, immo, cv, rightSpacer2);
+
+		// Créer des labels pour le footer
+		Label aide = new Label("Aide");
+		Label faq = new Label("FAQ");
+		Label ml = new Label("Mentions légales");
+		Label poli = new Label("Politique de confidentialité");
+		aide.getStyleClass().add("labelfooter");
+		faq.getStyleClass().add("labelfooter");
+		ml.getStyleClass().add("labelfooter");
+		poli.getStyleClass().add("labelfooter");
+				
+		// Créer des régions flexibles pour le footer
+		Region leftSpacer3 = new Region();
+		Region rightSpacer3 = new Region();
+			
+		// Ajouter les labels au footer 
+		HBox.setHgrow(leftSpacer3, Priority.ALWAYS);
+		HBox.setHgrow(rightSpacer3, Priority.ALWAYS);
+		footer.getChildren().addAll(leftSpacer3, aide, faq, ml, poli, rightSpacer3);
+		
+		// Créer un label pour le titre du site 
+		Label titre = new Label("Le site de petites annonces et de forums de discussion");
+		titre.getStyleClass().add("labeltitre");
+		titre.setWrapText(true);
+		titre.setMaxWidth(400); // Définir une largeur fixe pour le Label
+				
+		// Ajouter les éléments au conteneur VBox de droite 
+		root3.getChildren().addAll(titre, add);
+		
+		// Créer une image 
+		Image im2 = new Image(getClass().getResourceAsStream("/Images/dressing.png"));
+		ImageView imView2 = new ImageView(im2);
+				
+		// Propriétés de l'image
+		imView2.setFitWidth(700);
+		imView2.setPreserveRatio(true);
+
+		// Ajouter les éléments au conteneur VBox de gauche 
+		root4.getChildren().add(imView2);
+		
+		// Ajouter les éléments au conteneur HBox central 
+		centre.getChildren().addAll(root4, root3);
 		
 		// Ajouter les éléments au conteneur VBox
-		root1.getChildren().addAll(root2, barNavigation, add);
+		root1.getChildren().addAll(root2, barNavigation, centre, footer);
 		
 		// Créer la scène
 		Scene scene = new Scene(root1, 1500, 700);
